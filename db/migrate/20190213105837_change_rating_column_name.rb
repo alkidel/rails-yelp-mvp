b8f18a2b3797205488296ef915ef47b05c80a5b6
Alki Delichatsios <alkidel@gmail.com>
2019-02-13 16:01:33 +0100

    rails yelp mvp

---
+++ db/migrate/20190213105837_change_rating_column_name.rb
@@ -0,0 +1,5 @@
+class ChangeRatingColumnName < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :reviews, :stars, :rating
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Alki Pizza',
+    address:      'Ledru Rollin',
+    phone_number:  '+33 06 00 00 00 00',
+    category:      'italian'
+  },
+  {
+    name:         'Hello Chinese',
+    address:      'Oberkampf',
+    phone_number:  '+33 06 05 00 00 00',
+    category:      'chinese'
+  },
+  {
+    name:         'Belgian Waffles',
+    address:      'Boston',
+    phone_number:  '+1 00 00 00 00 00',
+    category:      'belgian'
+  },
+  {
+    name:         'Poopoo French',
+    address:      'San Francisco',
+    phone_number:  '+1 04 00 00 00 00',
+    category:      'french'
+  },
+  {
+    name:         'Fukushima',
+    address:      'Athens',
+    phone_number:  '+30 00 00 00 00 00',
+    category:      'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
